7e605221169a268fd94e7385be727894915c5085
Aatika Khan <aatika@easeyourlife.in>
2022-03-25 16:47:35 +0530

    shared pref for caching data

--- lib/data_provider.dart
+++ lib/data_provider.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'data_repository.dart';
 
@@ -10,7 +11,8 @@ class DataProvider with ChangeNotifier {
   List<DataModel>? data;
 
   Future<List<DataModel>?> getData() async {
-   
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
     // please add username and password for doing basic auth based request
     Response res = await http.get(
       Uri.parse('https://api.github.com/orgs/octokit/repos'),
@@ -22,8 +24,19 @@ class DataProvider with ChangeNotifier {
     if (res.statusCode == 200) {
       List<dynamic> body = jsonDecode(res.body);
       data = [];
-    //  add random data to list
+      //  add random data to list
       data = body.map((item) => DataModel.fromJson(item)).toList();
+
+      sharedPreferences.setString('repository_data', jsonEncode(body));
+
+      // Read the data, decode it and store it in map structure
+      List<dynamic> jsondata =
+          jsonDecode(sharedPreferences.getString('repository_data')!);
+      //convert it into required object
+      for (var value in jsondata) {
+        DataModel prefsData = DataModel.fromJson(value);
+        data!.add(prefsData);
+      }
     } else {
       throw "Unable to retrieve data. ${res.body}";
     }
