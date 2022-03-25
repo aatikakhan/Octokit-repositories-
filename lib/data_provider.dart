51023dcefd26aa511a8d489677606fe483b34224
Aatika Khan <aatika@easeyourlife.in>
2022-03-25 15:55:40 +0530

    get request using basic auth

--- lib/data_provider.dart
+++ lib/data_provider.dart
@@ -10,18 +10,22 @@ class DataProvider with ChangeNotifier {
   List<DataModel>? data;
 
   Future<List<DataModel>?> getData() async {
-    Response res =
-        await http.get(Uri.parse('https://api.github.com/orgs/octokit/repos'));
+   
+    // please add username and password for doing basic auth based request
+    Response res = await http.get(
+      Uri.parse('https://api.github.com/orgs/octokit/repos'),
+      headers: <String, String>{
+        'authorization':
+            'Basic ' + base64Encode(utf8.encode('username:password'))
+      },
+    );
     if (res.statusCode == 200) {
       List<dynamic> body = jsonDecode(res.body);
-      data = body
-          .map(
-            (item) => DataModel.fromJson(item),
-          )
-          .toList();
-      return data;
+      data = [];
+    //  add random data to list
+      data = body.map((item) => DataModel.fromJson(item)).toList();
     } else {
-      throw "Unable to retrieve posts.";
+      throw "Unable to retrieve data. ${res.body}";
     }
   }
 }
